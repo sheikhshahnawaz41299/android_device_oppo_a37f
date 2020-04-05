2991080d0baf37df674fbad83b5f6298dd46815c
sheikhshahnawaz41299 <sheikhshahnawaz5555@gmail.com>
2020-04-05 20:37:58 +0530

    fix build

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a37f-user \
+    lineage_a37f-userdebug \
+    lineage_a37f-eng
--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
- 
-add_lunch_combo lineage_a37f-userdebug
-add_lunch_combo lineage_a37f-user
-add_lunch_combo lineage_a37f-eng
